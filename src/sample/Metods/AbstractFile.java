1a402cf3097657e840b031563c09e0bd13996b01
RinaKoner128 <rinakoner65@gmail.com>
2020-05-31 21:50:25 +0300

    Initial commit

---
+++ src/sample/Metods/AbstractFile.java
@@ -0,0 +1,5 @@
+package sample.Metods;
+
+interface AbstractFile {
+    void ls();
+}
---
+++ src/sample/Metods/File.java
@@ -0,0 +1,13 @@
+package sample.Metods;
+
+class File implements AbstractFile {
+    private String name;
+
+    public File(String name) {
+        this.name = name;
+    }
+
+    public void ls() {
+        System.out.println(CompositeDemo.compositeBuilder + name);
+    }
+}
